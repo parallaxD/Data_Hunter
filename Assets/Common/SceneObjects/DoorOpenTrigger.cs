31fef8d3cf7df367c9a6c0c036d97ae787f671f3
parallaxD <denis_kuplevatskiy@mail.ru>
2024-06-17 23:36:22 +0500

    IssuesFixes

--- Assets/Common/SceneObjects/DoorOpenTrigger.cs
+++ Assets/Common/SceneObjects/DoorOpenTrigger.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorOpenTrigger : MonoBehaviour
+public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject _doorToOpen;
+    [SerializeField] private float _animSpeed;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CharacterController>() != null)
         {
-            _doorToOpen.GetComponent<Animation>().Play();
+            var animator = _doorToOpen.GetComponent<Animator>();
+            animator.speed = _animSpeed;
+            animator.Play("PersonDoorOpen");
             Destroy(gameObject);
         }
     }
